30fa5db6ec15d7f55eb415d182821a27a72992a1
SashaShulga94 <alexshulga.ss@gmail.com>
2022-12-17 21:36:01 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+hello = 'heloo on';
+console.log(hello);
 console.log(galleryItems);
