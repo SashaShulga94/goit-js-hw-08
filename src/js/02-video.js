69a754c55e1998edf766c5e09c7d0ee6e06cf6f4
SashaShulga94 <alexshulga.ss@gmail.com>
2022-12-19 14:23:15 +0200

    task2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,21 @@
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on(`timeupdate`, throttle(currentTimeOnVideo, 1000));
+player
+  .setCurrentTime(localStorage.getItem(VIDEOPLAYER_CURRENT_TIME))
+  .then(localStorage.getItem(VIDEOPLAYER_CURRENT_TIME))
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+    }
+  });
+
+function currentTimeOnVideo(e) {
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, JSON.stringify(e.seconds));
+}
