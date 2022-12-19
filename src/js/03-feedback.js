20a628f6c75300379b5aa4ace51a2c49ca775b3a
SashaShulga94 <alexshulga.ss@gmail.com>
2022-12-19 16:35:37 +0200

    3task complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const FEEDBACK_FORM = 'feedback-form-state';
+const { email, message } = feedbackForm;
+
+feedbackForm.addEventListener('input', throttle(getInputData, 500));
+feedbackForm.addEventListener('submit', saveInputData);
+
+readInputData();
+
+function getInputData(e) {
+  localStorage.setItem(
+    FEEDBACK_FORM,
+    JSON.stringify({
+      email: email.value,
+      message: message.value,
+    })
+  );
+}
+
+function readInputData() {
+  const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM));
+  console.log(savedData);
+  if (savedData) {
+    email.value = savedData.email;
+    message.value = savedData.message;
+  }
+  //   console.log('email', email.value);
+  //   console.log('message', message.value);
+}
+
+function saveInputData(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM);
+}
